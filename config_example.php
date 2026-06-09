add18e637e9782c293a6f139e6e5ecc1920865f1
Bartolomue2008 <daud94421@gmail.com>
2026-06-09 12:18:28 +0700

    update pengurus

---
+++ config_example.php
@@ -0,0 +1,8 @@
+<?php
+$koneksi = mysqli_connect(
+"localhost",
+"2526_12", // Sesuaikan dengan user hosting kamu jika di-online-kan
+"12345678",     // Sesuaikan dengan password hosting kamu jika di-online-kan
+"2526_12db",
+);
+?>
